224dab42f12ad710fafcfb62ffaab237ed693f48
Asad Khan <asad_khan47@hotmail.co.uk>
2020-04-05 19:36:57 +0100

    Updated style. Now hides and shows after a message is created

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 document
   .querySelector("form")
   .addEventListener("submit", event => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const messageInput = document.querySelector("#message-input");
-  const encrypted = btoa(messageInput.value);
-  
-  const linkInput = document.querySelector("#link-input");
-  linkInput.value = `${window.location}` + `#${encrypted}`;
-});
+    document.querySelector("#message-form").classList.add("hide");
+    document.querySelector("#link-div").classList.remove("hide");
+
+    const messageInput = document.querySelector("#message-input");
+    const encrypted = btoa(messageInput.value);
+
+    const linkInput = document.querySelector("#link-input");
+    linkInput.value = `${window.location}` + `#${encrypted}`;
+  });
 
 function copyTextFunction() {
   var copyText = document.getElementById("link-input")
